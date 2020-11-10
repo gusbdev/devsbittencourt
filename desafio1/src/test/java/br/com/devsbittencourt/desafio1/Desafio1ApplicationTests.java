342bd80bcc761569b380a35894d0f5dc04ec9436
Gustavo Barbosa <gusbdev@gmail.com>
2020-11-10 20:51:21 -0300

    First commit

---
+++ desafio1/src/test/java/br/com/devsbittencourt/desafio1/Desafio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsbittencourt.desafio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
