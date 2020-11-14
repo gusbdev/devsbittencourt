8ddf0560e192f62b1bcefe50051b5723d3e13c6f
Gustavo Barbosa <gusbdev@gmail.com>
2020-11-14 10:31:04 -0300

    Vehicle class

---
+++ desafio1/src/main/java/br/com/devsbittencourt/desafio1/models/Vehicle.java
@@ -0,0 +1,31 @@
+package br.com.devsbittencourt.desafio1.models;
+
+public class Vehicle {
+    private String marca;
+    private String modelo;
+    private String cor;
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
