1bef22762793e09a3d9ed2127303966b7c498047
Gustavo Barbosa <gusbdev@gmail.com>
2020-11-14 10:36:25 -0300

    Car, Moto classes

---
+++ desafio1/src/main/java/br/com/devsbittencourt/desafio1/models/Car.java
@@ -0,0 +1,13 @@
+package br.com.devsbittencourt.desafio1.models;
+
+public class Car extends Vehicle {
+    private int portas;
+
+    public int getPortas() {
+        return this.portas;
+    }
+
+    public void setPortas(int portas) {
+        this.portas = portas;
+    }
+}
---
+++ desafio1/src/main/java/br/com/devsbittencourt/desafio1/models/Moto.java
@@ -0,0 +1,13 @@
+package br.com.devsbittencourt.desafio1.models;
+
+public class Moto extends Vehicle {
+    private double cilindrada;
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+}
